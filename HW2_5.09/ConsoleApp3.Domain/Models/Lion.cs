29a19909f10849959079f6d0221698cde0818338
makelorig <106504877+makelorig@users.noreply.github.com>
2024-09-19 11:54:33 +0300

    Add files via upload

---
+++ HW2_5.09/ConsoleApp3.Domain/Models/Lion.cs
@@ -0,0 +1,18 @@
+﻿namespace Zoo_Animals_project.Domain.Models
+{
+    public class Lion : Animal {
+        private int _prideSize;
+
+        public Lion(string name, int age, Species species, int prideSize) : base(name, age, species) {
+            _prideSize = prideSize;
+        }
+
+        public void Hunt() {
+            Console.WriteLine("Охочусь");
+        }
+
+        public override void MakeSound() {
+            Console.WriteLine("Рычу");
+        }
+    }
+}
---
+++ HW2_5.09/ConsoleApp3.Domain/Models/Monkey.cs
@@ -0,0 +1,16 @@
+﻿namespace Zoo_Animals_project.Domain.Models
+{
+    public class Monkey : Animal {
+        private bool _canClimb;
+
+        public Monkey(string name, int age, Species species, bool canClimb) : base(name, age, species) {
+            _canClimb = canClimb;
+        }
+
+        
+        public override void MakeSound() {
+
+            Console.WriteLine("Ржу");
+        }
+    }
+}
