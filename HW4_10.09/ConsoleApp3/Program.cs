ab13978fed2175b3114ff548d12e1ec385f5c1dc
makelorig <106504877+makelorig@users.noreply.github.com>
2024-09-27 23:19:17 +0300

    Add files via upload

---
+++ HW4_10.09/ConsoleApp3/Program.cs
@@ -0,0 +1,36 @@
+﻿using Zoo_Animals_project.Domain.Models;
+
+Lion lion =  new Lion("lion", 10, Species.Birds, 10);
+
+lion.Hunt();
+
+Elephant elephant = new Elephant("elephant", 10, Species.Reptiles, 10);
+
+elephant.Stand();
+
+Monkey monkey = new Monkey("monkey", 10, Species.Mammal, true);
+monkey.Climb();
+Console.WriteLine("\n\n");
+
+Zoo zoo = new Zoo();
+zoo.SetMaxCapZoo(3);
+
+zoo.AddAnimal(lion);
+zoo.AddAnimal(elephant);
+zoo.AddAnimal(monkey);
+zoo.FeedAll();
+zoo.SoundOff();
+
+zoo.ZooInfo();
+Console.WriteLine("\n\n");
+zoo.animalFoundByType(Species.Mammal);
+Console.WriteLine("\n\n");
+Monkey mm = new Monkey("monkey", 10, Species.Mammal, true);
+zoo.AddAnimal(mm);
+zoo.AddAnimal(mm);
+zoo.AddAnimal(mm);
+zoo.AddAnimal(mm);
+
+
+
+
